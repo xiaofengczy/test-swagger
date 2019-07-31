84dd51cc910d7dddd714885af41185611f31bf62
test-mayun <465208000@qq.com>
2019-07-31 21:21:51 +0800

    Initial commit

---
+++ test-mayun/src/main/java/com/test/mayun/testmayun/TestMayunApplication.java
@@ -0,0 +1,13 @@
+package com.test.mayun.testmayun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMayunApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestMayunApplication.class, args);
+  }
+
+}
